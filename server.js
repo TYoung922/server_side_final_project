e05ad981005d4e6b752bce4e94464be324c30396
TercanoNuruva <thyoung89@gmail.com>
2024-07-02 12:35:08 -0600

    setup base pages

--- server.js
+++ server.js
@@ -1,13 +1,52 @@
 require("dotenv").config();
 
+const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const methodOverride = require("method-override");
+const session = require("express-session");
 
-app.use("/", require("./routes/router_index"));
+const connectDB = require("./server/config/db");
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+
+//admin
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  })
+);
+
+// Duplicate your route file below to also have a admin instead of main.
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
+
+// const insertMovieData = require("./server/routes/dummydate");
+// insertMovieData();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
